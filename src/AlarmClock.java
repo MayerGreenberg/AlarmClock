13aa07d2d46396d38347b652d0e5b589d90a8bd9
Mayer Greenberg <mgreenbe17@student.touro.edu>
2022-12-10 21:06:04 -0500

    added random pick of mp3

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -2,10 +2,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JDialog;
@@ -19,7 +24,9 @@ public class AlarmClock {
 		boolean test = false;
 		for (int i = 0; i < Integer.MAX_VALUE; i++) {
 			AlarmTimes.setAlarmTimes();
-			System.out.println(i + " --- " + new Date() + "\n" + AlarmTimes.alarmStart + "-->" + AlarmTimes.alarmStop);
+			System.out.println(i + " --- " + new Date() + "\n" + AlarmTimes.alarmStart + "-->" + AlarmTimes.alarmStop
+					+ "\nAlarm will sound in " + 
+					AlarmTimes.hms(LocalTime.now().until(AlarmTimes.alarmStart, ChronoUnit.SECONDS))+ "\n");
 			if (AlarmTimes.isAlarmTime(test)) {
 
 				AlarmSounds.soundAlarm(test);
@@ -30,12 +37,11 @@ public class AlarmClock {
 					System.exit(0);
 				}
 			} else
-				TimeUnit.SECONDS.sleep(60);
+				TimeUnit.SECONDS.sleep(1);
 		}
 	}
 }
 
-// java.awt.Toolkit.getDefaultToolkit().beep();
 class AlarmVisual {
 	public static boolean alert(String title, String message, int timeout) {
 		JLabel lbmsg = new JLabel(message);
@@ -78,16 +84,39 @@ class AlarmSounds {
 	public static boolean isLinux() {
 		return System.getProperty("os.name") == "Linux";
 	}
-
-	public static void soundAlarm(boolean test) throws IOException {
-		if (test)
+	public static ArrayList<String> getMp3s() {
+		File folder = new File(System.getProperty("user.dir"));
+		File[] listOfFiles = folder.listFiles();
+		ArrayList<String> mp3s = new ArrayList<String>();
+		for (int i = 0; i < listOfFiles.length; i++) 
+			if (listOfFiles[i].isFile())
+				if (listOfFiles[i].getName().endsWith(".mp3"))
+					mp3s.add(listOfFiles[i].getName());
+		return mp3s;
+	}
+	public static String pickMp3(ArrayList<String> mp3List) {
+		Random r = new Random();
+		int low = 0;
+		int high = mp3List.size();
+		int result = r.nextInt(high - low) + low;
+		return mp3List.get(result);
+	}
+	public static int soundAlarm(boolean test) throws IOException {
+		if (test) {
 			java.awt.Toolkit.getDefaultToolkit().beep();
-		else if (AlarmSounds.isLinux())
-			Runtime.getRuntime().exec("vlc andrew-schultz.mp3");
+			return 60;
+		}
 		else {
-			ProcessBuilder builder = new ProcessBuilder();
-			builder.command("cmd.exe", "/c", "start vlc andrew-schultz.mp3");
-			Process process = builder.start();
+			ArrayList<String> mp3List = getMp3s();
+			String mp3 = pickMp3(mp3List);
+			if (AlarmSounds.isLinux())
+				Runtime.getRuntime().exec("vlc " + mp3);
+			else {
+				ProcessBuilder builder = new ProcessBuilder();
+				builder.command("cmd.exe", "/c", "start vlc " + mp3);
+				Process process = builder.start();
+			}
+			return mp3.startsWith("and") ? 50 : 264;//temporary fix until i get length of mp3s
 		}
 	}
 
@@ -106,6 +135,13 @@ class AlarmTimes {
 	static LocalTime alarmStart;
 	static LocalTime alarmStop;
 
+	public static String hms(long seconds) {
+		long hours = seconds / 3600;
+		long minutes = (seconds % 3600) / 60;
+		seconds = seconds % 60;
+
+		return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+	}
 	public static boolean isAlarmTime(boolean test) {
 		if (test)
 			return true;
